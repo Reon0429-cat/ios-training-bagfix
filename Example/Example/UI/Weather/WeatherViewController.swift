f7a74a75f7bcdb960b95de687fe3abb8ed660b71
OONISHI REON <oonishireon110@gmail.com>
2022-05-20 18:50:54 +0900

    WeatherViewControllerのdeinitが呼ばれない不具合を修正

--- Example/Example/UI/Weather/WeatherViewController.swift
+++ Example/Example/UI/Weather/WeatherViewController.swift
@@ -44,14 +44,16 @@ class WeatherViewController: UIViewController {
     
     @IBAction func loadWeather(_ sender: Any?) {
         self.activityIndicator.startAnimating()
-        weatherModel.fetchWeather(at: "tokyo", date: Date()) { result in
+        weatherModel.fetchWeather(at: "tokyo", date: Date()) { [weak self] result in
+            guard let strongSelf = self else { return }
             DispatchQueue.main.async {
-                self.activityIndicator.stopAnimating()
-                self.handleWeather(result: result)
+                strongSelf.activityIndicator.stopAnimating()
+                strongSelf.handleWeather(result: result)
             }
         }
-        disasterModel.fetchDisaster { (disaster) in
-            self.disasterLabel.text = disaster
+        disasterModel.fetchDisaster { [weak self] (disaster) in
+            guard let strongSelf = self else { return }
+            strongSelf.disasterLabel.text = disaster
         }
     }
     
